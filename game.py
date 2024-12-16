f7a0fcf46308d832a045af5a8cac4b8c849881d4
Dileep <ravindraprasadprasad569@gmail.com>
2024-12-16 16:58:01 +0530

    commited

--- game.py
+++ game.py
@@ -1,71 +1,54 @@
 import random
 
-# Print multiline instruction
-print('Winning rules of the game ROCK PAPER SCISSORS are:\n'
-      + "Rock vs Paper -> Paper wins \n"
-      + "Rock vs Scissors -> Rock wins \n"
-      + "Paper vs Scissors -> Scissors wins \n")
+def print_rules():
+    print("Winning Rules of the Rock Paper Scissors game:")
+    print("Rock vs paper -> paper wins")
+    print("Rock vs scissor -> Rock wins")
+    print("Paper vs scissor -> scissor wins")
 
-while True:
-
-    print("Enter your choice \n 1 - Rock \n 2 - Paper \n 3 - Scissors \n")
-
-    # Take the input from user
-    choice = int(input("Enter your choice: "))
-
-    # Looping until user enters valid input
-    while choice > 3 or choice < 1:
-        choice = int(input('Enter a valid choice please : '))
-
-    # Initialize value of choice_name variable corresponding to the choice value
+def get_choice_name(choice):
     if choice == 1:
-        choice_name = 'Rock'
+        return 'Rock'
     elif choice == 2:
-        choice_name = 'Paper'
-    else:
-        choice_name = 'Scissors'
+        return 'Paper'
+    elif choice == 3:
+        return 'Scissors'
 
-    # Print user choice
-    print('User choice is:', choice_name)
-    print("Now it's Computer's Turn...")
+def game():
+    print_rules()
+    
+    while True:
+        # Ask user for their choice
+        user_choice = int(input("\nEnter your choice: 1 for Rock, 2 for Paper, 3 for Scissors: "))
+        
+        # Check if the user input is valid
+        if user_choice not in [1, 2, 3]:
+            print("Invalid choice, please enter 1, 2, or 3.")
+            continue
+        
+        user_choice_name = get_choice_name(user_choice)
+        print(f"\nYour choice is: {user_choice_name}")
+        
+        # Get the computer's choice using random number generator
+        comp_choice = random.randint(1, 3)
+        comp_choice_name = get_choice_name(comp_choice)
+        print(f"Computer's choice is: {comp_choice_name}")
+        
+        # Determine the winner
+        if user_choice == comp_choice:
+            print("It's a tie!")
+        elif (user_choice == 1 and comp_choice == 2) or \
+             (user_choice == 2 and comp_choice == 3) or \
+             (user_choice == 3 and comp_choice == 1):
+            print("Computer wins!")
+        else:
+            print("You win!")
+        
+        # Ask if the user wants to play again
+        play_again = input("\nDo you want to play again? (yes/no): ").lower()
+        if play_again != 'yes':
+            print("Thanks for playing!")
+            break
 
-    # Computer chooses randomly any number among 1, 2, and 3
-    comp_choice = random.randint(1, 3)
-
-    # Initialize value of comp_choice_name variable corresponding to the choice value
-    if comp_choice == 1:
-        comp_choice_name = 'Rock'
-    elif comp_choice == 2:
-        comp_choice_name = 'Paper'
-    else:
-        comp_choice_name = 'Scissors'
-
-    print("Computer choice is:", comp_choice_name)
-    print(choice_name, 'vs', comp_choice_name)
-
-    # Determine the winner
-    if choice == comp_choice:
-        result = "DRAW"
-    elif (choice == 1 and comp_choice == 2) or (comp_choice == 1 and choice == 2):
-        result = 'Paper'
-    elif (choice == 1 and comp_choice == 3) or (comp_choice == 1 and choice == 3):
-        result = 'Rock'
-    elif (choice == 2 and comp_choice == 3) or (comp_choice == 2 and choice == 3):
-        result = 'Scissors'
-
-    # Print the result
-    if result == "DRAW":
-        print("<== It's a tie! ==>")
-    elif result == choice_name:
-        print("<== User wins! ==>")
-    else:
-        print("<== Computer wins! ==>")
-
-    # Ask if the user wants to play again
-    print("Do you want to play again? (Y/N)")
-    ans = input().lower()
-    if ans == 'n':
-        break
-
-# After coming out of the while loop, print thanks for playing
-print("Thanks for playing!")
+# Start the game
+game()
